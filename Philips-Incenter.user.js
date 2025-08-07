5a8f95df7cf11da4dc6f0275327fbb271ce41694
Igor <igor.lovric@gmail.com>
2025-08-07 15:00:13 +0200

    Remove jQuery dependency Add "Buttons enabled" message

--- Philips-Incenter.user.js
+++ Philips-Incenter.user.js
@@ -1,38 +1,73 @@
 // ==UserScript==
-// @name         Philips - InCenter - Enable additional PDF buttons jQuery
+// @name         Philips - InCenter - Enable additional PDF buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0.0_2025-06-17
+// @version      1.1.0.0_2025-08-07
 // @description  Enable Open, Save, Print, Text and Draw buttons
 // @author       Igor Lovrić
 // @match        https://philips.mizecx.com/knowledgeDocument.html*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mizecx.com
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var interval=setInterval(function() {
-        if ($('#download', $('#docFrame').contents()).length>0) {
-            clearInterval(interval);
-            // console.log('FOUND!');
-            var elements=[
-                '#toolbarViewerRight #openFile',
-                '#toolbarViewerRight #print',
-                '#toolbarViewerRight #download',
-                '#toolbarViewerRight #editorFreeText',
-                '#toolbarViewerRight #editorInk',
-                '#secondaryToolbarButtonContainer #secondaryDownload',
-                '#secondaryToolbarButtonContainer #secondaryPrint',
-                '#secondaryToolbarButtonContainer #secondaryOpenFile',
-                '#secondaryToolbarButtonContainer .horizontalToolbarSeparator'
-            ];
-            elements.forEach(function(e) {
-                $(e, $('#docFrame').contents()).removeClass('hiddenMediumView hiddenLargeView visibleMediumView visibleLargeView hidden');
-            });
-        } else {
-            // console.log('NOT FOUND!');
+    var interval = setInterval(function () { // Wait for PDF to load
+        var docFrame = document.getElementById('docFrame');
+        if (docFrame && docFrame.contentDocument) {
+            var downloadButton = docFrame.contentDocument.querySelector('#download');
+            if (downloadButton) {
+                clearInterval(interval);
+
+                var selectors = [
+                    '#toolbarViewerRight #openFile',
+                    '#toolbarViewerRight #print',
+                    '#toolbarViewerRight #download',
+                    '#toolbarViewerRight #editorFreeText',
+                    '#toolbarViewerRight #editorInk',
+                    '#secondaryToolbarButtonContainer #secondaryDownload',
+                    '#secondaryToolbarButtonContainer #secondaryPrint',
+                    '#secondaryToolbarButtonContainer #secondaryOpenFile',
+                    '#secondaryToolbarButtonContainer .horizontalToolbarSeparator'
+                ];
+
+                selectors.forEach(function (selector) {
+                    var elements = docFrame.contentDocument.querySelectorAll(selector);
+                    elements.forEach(function (el) {
+                        el.classList.remove(
+                            'hiddenMediumView',
+                            'hiddenLargeView',
+                            'visibleMediumView',
+                            'visibleLargeView',
+                            'hidden'
+                        );
+                    });
+                });
+
+
+                var toolbars = docFrame.contentDocument.querySelectorAll('#toolbarViewerRight');
+
+                if (toolbars) {
+                    toolbars.forEach(function(toolbar) {
+                        var span = document.createElement('span');
+                        span.style.paddingTop = '7px';
+                        span.style.color = '#aa0000';
+                        span.textContent = 'Buttons enabled →';
+                        span.style.transition = 'opacity 1s ease'; // Enable smooth fade out
+                        span.style.opacity = '1'; // Start fully visible
+
+                        toolbar.insertBefore(span, toolbar.firstChild);
+                        setTimeout(function() {
+                            span.style.opacity = '0';
+                        }, 5000);
+                        setTimeout(function() {
+                            if (span.parentNode) {
+                                span.parentNode.removeChild(span);
+                            }
+                        }, 6000);
+                    });
+                }
+            }
         }
     }, 1000);
 })();
